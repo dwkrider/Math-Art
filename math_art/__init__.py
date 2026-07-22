68b6d76fafda924b7e048dfceab9bf1722543864
David Krider <13036234+dwkrider@users.noreply.github.com>
2026-07-22 16:42:40 -0700

    Organize the Math Art menu into six themed submenus
    Minimal Surfaces (Scherk-Collins, parametric, TPMS, knot span,
    Plateau span), Polyhedra (regular solids, Conway, zonohedra,
    Waterman, symmetrohedra, 4D polytopes), Fractals (sponges, fractal
    polyhedra, space-filling curves), Knots & Curves (prime knots,
    Seifert surfaces, strange attractors, dual helix), Weaves & Tangles
    (polylinks, tangles, weaves, rotegrity), and Sculpture (Platonic
    twist, twisted torus, oloid, symmetric sculpture, Leonardo style),
    replacing the former flat 26-entry list.

--- math_art/__init__.py
+++ math_art/__init__.py
@@ -54,52 +54,111 @@ _MODULES = (scherk_collins_generator, minimal_surface_toolkit,
             dual_helix_generator)
 
 
-class VIEW3D_MT_math_art_add(bpy.types.Menu):
-    bl_idname = "VIEW3D_MT_math_art_add"
-    bl_label = "Math Art"
+class VIEW3D_MT_math_art_minimal(bpy.types.Menu):
+    bl_idname = "VIEW3D_MT_math_art_minimal"
+    bl_label = "Minimal Surfaces"
 
     def draw(self, context):
         lay = self.layout
         lay.operator_menu_enum("mesh.scherk_collins_add", "preset",
                                text="Scherk-Collins Sculpture",
                                icon='MESH_TORUS')
-        lay.separator()
         lay.operator("mesh.parametric_minimal_add",
                      icon='SURFACE_NSPHERE')
         lay.operator("mesh.tpms_add", icon='MESH_ICOSPHERE')
         lay.operator("mesh.minimal_knot_span_add", icon='MESH_TORUS')
-        lay.operator("object.minimal_span", icon='OUTLINER_OB_SURFACE')
-        lay.separator()
-        lay.operator("mesh.seifert_surface_add", icon='MOD_SIMPLIFY')
-        lay.separator()
+        lay.operator("object.minimal_span",
+                     icon='OUTLINER_OB_SURFACE')
+
+
+class VIEW3D_MT_math_art_polyhedra(bpy.types.Menu):
+    bl_idname = "VIEW3D_MT_math_art_polyhedra"
+    bl_label = "Polyhedra"
+
+    def draw(self, context):
+        lay = self.layout
+        lay.operator("mesh.regular_solid_add", icon='MESH_ICOSPHERE')
         lay.operator("mesh.conway_add", icon='MESH_ICOSPHERE')
         lay.operator("mesh.zonohedron_add", icon='MESH_UVSPHERE')
         lay.operator("mesh.waterman_add", icon='MESH_ICOSPHERE')
-        lay.operator("mesh.rotegrity_add", icon='SPHERE')
-        lay.operator("mesh.poly_weave_add", icon='MOD_LATTICE')
-        lay.separator()
-        lay.operator("mesh.polylinks_add", icon='MESH_CIRCLE')
-        lay.operator("mesh.platonic_twist_add", icon='MOD_SCREW')
+        lay.operator("mesh.symmetrohedron_add",
+                     icon='MESH_ICOSPHERE')
+        lay.operator("mesh.polytope4d_add", icon='MESH_CUBE')
+
+
+class VIEW3D_MT_math_art_fractals(bpy.types.Menu):
+    bl_idname = "VIEW3D_MT_math_art_fractals"
+    bl_label = "Fractals"
+
+    def draw(self, context):
+        lay = self.layout
+        lay.operator("mesh.sponge_add", icon='MESH_CUBE')
         lay.operator("mesh.fractal_polyhedron_add",
                      icon='OUTLINER_OB_POINTCLOUD')
-        lay.operator("mesh.symmetrohedron_add", icon='MESH_ICOSPHERE')
-        lay.operator("mesh.twisted_torus_add", icon='MESH_TORUS')
-        lay.operator("mesh.polytope4d_add", icon='MESH_CUBE')
-        lay.operator("mesh.tangle_add", icon='MOD_BOOLEAN')
-        lay.operator("mesh.sponge_add", icon='MESH_CUBE')
         lay.operator("curve.space_filling_add", icon='CURVE_DATA')
-        lay.operator("mesh.oloid_add", icon='MESH_CAPSULE')
+
+
+class VIEW3D_MT_math_art_knots(bpy.types.Menu):
+    bl_idname = "VIEW3D_MT_math_art_knots"
+    bl_label = "Knots && Curves"
+
+    def draw(self, context):
+        lay = self.layout
         lay.operator("curve.prime_knot_add", icon='FORCE_VORTEX')
-        lay.operator("mesh.regular_solid_add", icon='MESH_ICOSPHERE')
-        lay.operator("curve.dual_helix_add", icon='MOD_SCREW')
+        lay.operator("mesh.seifert_surface_add", icon='MOD_SIMPLIFY')
         lay.operator_menu_enum("curve.attractor_add", "preset",
                                text="Strange Attractor",
                                icon='RNDCURVE')
-        lay.separator()
+        lay.operator("curve.dual_helix_add", icon='MOD_SCREW')
+
+
+class VIEW3D_MT_math_art_weaves(bpy.types.Menu):
+    bl_idname = "VIEW3D_MT_math_art_weaves"
+    bl_label = "Weaves && Tangles"
+
+    def draw(self, context):
+        lay = self.layout
+        lay.operator("mesh.polylinks_add", icon='MESH_CIRCLE')
+        lay.operator("mesh.tangle_add", icon='MOD_BOOLEAN')
+        lay.operator("mesh.poly_weave_add", icon='MOD_LATTICE')
+        lay.operator("mesh.rotegrity_add", icon='SPHERE')
+
+
+class VIEW3D_MT_math_art_sculpture(bpy.types.Menu):
+    bl_idname = "VIEW3D_MT_math_art_sculpture"
+    bl_label = "Sculpture"
+
+    def draw(self, context):
+        lay = self.layout
+        lay.operator("mesh.platonic_twist_add", icon='MOD_SCREW')
+        lay.operator("mesh.twisted_torus_add", icon='MESH_TORUS')
+        lay.operator("mesh.oloid_add", icon='MESH_CAPSULE')
         lay.operator_menu_enum("object.symmetric_sculpture_add",
                                "preset", text="Symmetric Sculpture",
                                icon='MOD_MIRROR')
         lay.operator("object.leonardo_add", icon='MESH_ICOSPHERE')
+
+
+class VIEW3D_MT_math_art_add(bpy.types.Menu):
+    bl_idname = "VIEW3D_MT_math_art_add"
+    bl_label = "Math Art"
+
+    def draw(self, context):
+        lay = self.layout
+        lay.menu("VIEW3D_MT_math_art_minimal",
+                 icon='SURFACE_NSPHERE')
+        lay.menu("VIEW3D_MT_math_art_polyhedra",
+                 icon='MESH_ICOSPHERE')
+        lay.menu("VIEW3D_MT_math_art_fractals", icon='MESH_CUBE')
+        lay.menu("VIEW3D_MT_math_art_knots", icon='FORCE_VORTEX')
+        lay.menu("VIEW3D_MT_math_art_weaves", icon='MOD_LATTICE')
+        lay.menu("VIEW3D_MT_math_art_sculpture", icon='MOD_MIRROR')
+
+
+_MENUS = (VIEW3D_MT_math_art_minimal, VIEW3D_MT_math_art_polyhedra,
+          VIEW3D_MT_math_art_fractals, VIEW3D_MT_math_art_knots,
+          VIEW3D_MT_math_art_weaves, VIEW3D_MT_math_art_sculpture,
+          VIEW3D_MT_math_art_add)
 
 
 def _menu_func(self, context):
@@ -111,12 +170,16 @@ def register():
     for m in _MODULES:
         m.ADD_MENU = False       # entries live in the Math Art submenu
         m.register()
-    bpy.utils.register_class(VIEW3D_MT_math_art_add)
+    for c in _MENUS:
+        bpy.utils.register_class(c)
     bpy.types.VIEW3D_MT_add.append(_menu_func)
 
 
 def unregister():
     bpy.types.VIEW3D_MT_add.remove(_menu_func)
-    bpy.utils.unregister_class(VIEW3D_MT_math_art_add)
+    for c in reversed(_MENUS):
+        bpy.utils.unregister_class(c)
+    for m in reversed(_MODULES):
+        m.unregister()
     for m in reversed(_MODULES):
         m.unregister()
